0fb0e5919aee49c908b05ecc4b405020c76ba36e
Rick <1801210375@pku.edu.cn>
2019-04-01 16:55:49 +0800

    Implement two multiplexer to select registers.

--- verilog/cpu.v
+++ verilog/cpu.v
@@ -39,10 +39,36 @@ module cpu(
 	reg [7:0] add_buf; // address buffer
 	reg [7:0] data_buf; // data buffer
 	reg [7:0] cmd; // instruction
+	wire [7:0] reg_res0;
+	wire [7:0] reg_res1;
 
 	reg [7:0] op1; // operator 1
 	reg [7:0] op2; // operator 2
+	wire [7:0] op_res;
 
-	
+	alu alu_u(
+		.cmd(cmd[5:4]),
+		.op1(op1),
+		.op2(op2),
+		.result(op_res)
+	);
+
+	reg_mux reg_mux_u0(
+		.cmd(cmd[3:2]),
+		.r0(r0),
+		.r1(r1),
+		.r2(r2),
+		.r3(ip),
+		.result(reg_res0)
+	);
+
+	reg_mux reg_mux_u1(
+		.cmd(cmd[1:0]),
+		.r0(r0),
+		.r1(r1),
+		.r2(r2),
+		.r3(ip),
+		.result(reg_res1)
+	);
 
 endmodule
---
+++ verilog/reg_mux.v
@@ -0,0 +1,20 @@
+module reg_mux(cmd, r0, r1, r2, r3, result);
+
+	input [1:0] cmd;
+	input [7:0] r0;
+	input [7:0] r1;
+	input [7:0] r2;
+	input [7:0] r3;
+	output [7:0] result;
+
+	always @(*)
+		begin
+			case(cmd)
+			2'b00: result <= r0;
+			2'b01: result <= r1;
+			2'b10: result <= r2;
+			2'b11: result <= r3;
+			endcase
+		end
+
+endmodule
